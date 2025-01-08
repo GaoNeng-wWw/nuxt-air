8150c0750624ce8bb8daab43aa60ecb51e726854
GaoNeng-wWw <gaonenggg@outlook.com>
2025-01-08 14:30:53 +0800

    feat: add nuxt-auth-utils,prisma,tailwind,eslint,color-mode,i18n deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['nuxt-auth-utils', '@prisma/nuxt', '@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxtjs/color-mode', '@nuxtjs/i18n' ],
+  devtools: { enabled: true },
+  css: [
+    '~/assets/css/reset.css',
+  ],
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+  prisma: {
+    installStudio: false,
+    generateClient: true,
+  },
+  tailwindcss: {
+    config: {
+      content: [
+        './pages/**/*.vue',
+        './components/**/*.vue',
+        './contens/**/*.vue',
+      ],
+    },
+  },
+  colorMode: {
+    classSuffix: '',
+    storage: 'sessionStorage',
+    storageKey: '__COLOR_SCHEMA__',
+  },
 })
