6bd9750d3744fd30a9e5e799af67599bded36320
GaoNeng-wWw <gaonenggg@outlook.com>
2025-01-08 14:30:53 +0800

    feat: add nuxt-auth-utils,prisma,tailwind,eslint,color-mode,i18n deps

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt({
+  rules: {
+    'curly': 'off',
+    '@stylistic/brace-style': ['warn', '1tbs'],
+    'vue/multi-word-component-names': ['off'],
+  },
+})
