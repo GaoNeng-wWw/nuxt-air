5fb2c2529953329dba89e53e2c476fa2827b0c2b
GaoNeng-wWw <gaonenggg@outlook.com>
2025-01-12 21:34:45 +0800

    feat: popover basic layout

--- components/ui/dropdown-menu/DropdownMenuSubTrigger.vue
+++ components/ui/dropdown-menu/DropdownMenuSubTrigger.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-import { ChevronRight } from 'lucide-vue-next';
+import { cn } from '@/lib/utils'
+import { ChevronRight } from 'lucide-vue-next'
 import {
   DropdownMenuSubTrigger,
   type DropdownMenuSubTriggerProps,
   useForwardProps,
-} from 'radix-vue';
-import { computed, type HTMLAttributes } from 'vue';
-import { cn } from '@/lib/utils';
+} from 'radix-vue'
+import { computed, type HTMLAttributes } from 'vue'
 
-const props = defineProps<DropdownMenuSubTriggerProps & { class?: HTMLAttributes['class'] }>();
+const props = defineProps<DropdownMenuSubTriggerProps & { class?: HTMLAttributes['class'] }>()
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props;
+  const { class: _, ...delegated } = props
 
-  return delegated;
-});
+  return delegated
+})
 
-const forwardedProps = useForwardProps(delegatedProps);
+const forwardedProps = useForwardProps(delegatedProps)
 </script>
 
 <template>
---
+++ composables/useMarkdownParser.ts
@@ -0,0 +1,35 @@
+import { createMarkdownParser, createShikiHighlighter, rehypeHighlight } from '@nuxtjs/mdc/runtime';
+import MaterialThemeDark from 'shiki/themes/material-theme-darker.mjs'
+import MaterialThemeLight from 'shiki/themes/material-theme-ocean.mjs';
+
+export const useMarkdownParser = async () => {
+  let parser: Awaited<ReturnType<typeof createMarkdownParser>>
+  const langs = (await import('shiki/langs.mjs')).bundledLanguagesBase
+  const parse = async (markdown: string) => {
+    if (parser){
+      return parser(markdown);
+    }
+    parser = await createMarkdownParser({
+      remark: {
+        plugins:{
+          highlight: {
+            instance: rehypeHighlight,
+            options: {
+              theme: 'material-theme-palenight',
+              highlighter: createShikiHighlighter({
+                bundledThemes:{
+                  'material-theme-palenight': MaterialThemeLight
+                },
+                bundledLangs: {
+                  javascript: await import('shiki/langs/javascript.mjs')
+                }
+              })
+            }
+          }
+        }
+      }
+    })
+    return parser(markdown);
+  }
+  return parse;
+}
