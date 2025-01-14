cff5f74aac45a306325518eafdc292bf76375261
GaoNeng-wWw <gaonenggg@outlook.com>
2025-01-14 15:23:14 +0800

    feat: markdown sync scroll

--- components/ui/markdown-editor/utils/suggestion-factory.ts
+++
@@ -1,8 +0,0 @@
-import type { MentionNodeAttrs } from "@tiptap/extension-mention";
-import type { SuggestionOptions } from "@tiptap/suggestion";
-
-export const createSuggestion = <SuggestionItem,Attrs=MentionNodeAttrs>(
-  opts: Omit<SuggestionOptions<SuggestionItem, Attrs>, 'editor'>
-) => {
-  return opts as SuggestionOptions<SuggestionItem, Attrs>;
-}
