63667bae2f051528bae2269bf2d7c562ecf66073
GaoNeng-wWw <gaonenggg@outlook.com>
2025-01-23 15:52:20 +0800

    feat: useDialog hook

---
+++ components/float-button/avatar.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+import {User} from 'lucide-vue-next';
+const {render: r2, removeAll} = useDialog({
+  content: h('span',{onClick: onClickSpan},'hello world')
+})
+const {render} = useDialog({
+  content: h('span', {onClick: r2}, 'hello')
+})
+function onClickSpan(){
+  removeAll();
+}
+</script>
+<template>
+  <div>
+    <ui-avatar class="bg-gradient-to-tl from-slate-950 to-slate-800 border-border border">
+      <ui-avatar-fallback>
+        <user @click="render" />
+      </ui-avatar-fallback>
+    </ui-avatar>
+  </div>
+</template>
---
+++ composables/useDialog.ts
@@ -0,0 +1,99 @@
+import { isClient } from "@vueuse/core";
+import { DialogPortal } from "radix-vue";
+import { render, type VNode } from "vue";
+import Dialog from "~/components/ui/dialog/Dialog.vue";
+import DialogContent from "~/components/ui/dialog/DialogContent.vue";
+import DialogDescription from "~/components/ui/dialog/DialogDescription.vue";
+import DialogFooter from "~/components/ui/dialog/DialogFooter.vue";
+import DialogHeader from "~/components/ui/dialog/DialogHeader.vue";
+import DialogTitle from "~/components/ui/dialog/DialogTitle.vue"
+export type UseDialogOptions = {
+  title: VNode;
+  description: VNode,
+  content: VNode,
+  footer:VNode,
+}
+
+type DialogIntsnace = {
+  dialog: VNode,
+  anchor: HTMLDivElement
+}
+
+const dialogStack: DialogIntsnace[] = [];
+
+export const useDialog = (
+  opts: Partial<UseDialogOptions> = {}
+) => {
+  const {title,description,content,footer} = opts;
+  const removeByInstance = (instance: DialogIntsnace) => {
+    dialogStack
+    .filter(_instance => _instance === instance)
+    .forEach(({anchor}) => {
+      setTimeout(() => {
+        render(null, anchor);
+        anchor.remove();
+      }, 300);
+    })
+  }
+  function remove(){
+   removeByInstance(dialogStack[0]);
+   dialogStack.shift();
+  }
+  return {
+    remove,
+    removeAll: ()=>{
+      while (dialogStack.length){
+        const {anchor} = dialogStack.shift()!;
+        render(null,anchor);
+        anchor.remove();
+      }
+    },
+    render: ()=>{
+      if (!isClient){
+        return;
+      }
+      const dialogTtile = h(DialogTitle,title);
+      const dialogDescription = h(DialogDescription,description);
+      const dialogHeader = h(DialogHeader,[dialogTtile,dialogDescription]);
+      const dialogFooter = h(
+        DialogFooter,
+        footer
+      );
+      const dialogContent = h(
+        DialogContent,
+        [dialogHeader,content,dialogFooter]
+      );
+      const anchor = document.createElement('div');
+      anchor.style.position = 'fixed';
+      anchor.style.top = '0';
+      anchor.style.left = '0';
+      anchor.style.width = '100%';
+      anchor.style.height = '100%';
+      const dialogPortal = h(
+        DialogPortal, {
+          to: anchor,
+          forceMount: true,
+        },
+        [
+          dialogContent
+        ]
+      )
+      const dialog = h(
+        Dialog,
+        {
+          defaultOpen: true,
+          "onUpdate:open": (openState)=>{
+            if(!openState){
+              removeByInstance(instance)
+            }
+          },
+        },
+        dialogPortal
+      );
+      const instance = {dialog,anchor};
+      dialogStack.push(instance);
+      document.body.append(anchor);
+      render(dialog, anchor);
+    }
+  }
+}
