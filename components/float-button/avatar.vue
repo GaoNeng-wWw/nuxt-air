34c48686d5fabb4548c4a2c56d5c517b5b552547
GaoNeng-wWw <gaonenggg@outlook.com>
2025-01-23 21:06:59 +0800

    feat: oauth dialog

--- components/float-button/avatar.vue
+++ components/float-button/avatar.vue
@@ -1,20 +1,16 @@
 <script lang="ts" setup>
+import { LoginForm } from '#components';
 import {User} from 'lucide-vue-next';
-const {render: r2, removeAll} = useDialog({
-  content: h('span',{onClick: onClickSpan},'hello world')
-})
 const {render} = useDialog({
-  content: h('span', {onClick: r2}, 'hello')
+  content: h(LoginForm)
 })
-function onClickSpan(){
-  removeAll();
-}
+
 </script>
 <template>
   <div>
     <ui-avatar class="bg-gradient-to-tl from-slate-950 to-slate-800 border-border border">
-      <ui-avatar-fallback>
-        <user @click="render" />
+      <ui-avatar-fallback @click="render">
+        <user />
       </ui-avatar-fallback>
     </ui-avatar>
   </div>
