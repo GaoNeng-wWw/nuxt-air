8f6634b2898b5677ca515b73a147e8549c7e5eb4
GaoNeng-wWw <gaonenggg@outlook.com>
2025-01-26 15:17:07 +0800

    feat: 更新 Markdown 渲染组件，修复类名并添加返回按钮

--- pages/index/index/post/[id].vue
+++ pages/index/index/post/[id].vue
@@ -1,14 +1,17 @@
 <script lang="ts" setup>
 import {MarkdownRender} from '@/components/ui/markdown-render';
+import { ChevronLeft } from 'lucide-vue-next';
 const route = useRoute();
+const router = useRouter();
 const id = ref<string>(route.params.id.toString() ?? '');
 
-const {data, status, error} = await useAsyncData(`post-${id.value}`, ()=>$fetch(`/api/post/${id.value}`));
+const {data} = await useAsyncData(`post-${id.value}`, ()=>$fetch(`/api/post/${id.value}`));
 </script>
 
 <template>
     <div class="max-w-xl w-full h-auto px-2 sm:px-0 py-4 mx-auto mt-4">
       <markdown-render v-if="data?.post" :value="data.post.content" :tag="'article'" />
+      <chevron-left class="w-6 h-6 cursor-pointer underline" @click="router.back()" />
     </div>
 </template>
 
