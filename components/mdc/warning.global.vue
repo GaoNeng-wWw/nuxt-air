9462a45b835368fbfc5472de10437bf23526cca5
GaoNeng-wWw <gaonenggg@outlook.com>
2025-02-04 15:16:00 +0800

    feat: add (`bilibili` & `youtube`) video card, `Pull Request` card and `Warning` alert mdx component

---
+++ components/mdc/warning.global.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import {TriangleAlert} from 'lucide-vue-next';
+</script>
+
+<template>
+  <div class="w-full px-2 py-1 warning-alert flex flex-col rounded-md">
+    <div class="w-full flex items-center gap-2 text-xl">
+      <triangle-alert class="size-8 shrink-0 alert-icon" />
+      <span class="warning-alert__title break-all">
+        <slot name="title" />
+      </span>
+    </div>
+    <span class="break-all">
+      <slot>
+        <slot name="content" />
+      </slot>
+    </span>
+  </div>
+</template>
+
+<style scoped>
+.warning-alert {
+  background: theme('colors.orange.400/0.3');
+}
+.alert-icon {
+  color: theme('colors.orange.500');
+}
+.warning-alert__title{
+  color: theme('colors.orange.500');
+}
+</style>
