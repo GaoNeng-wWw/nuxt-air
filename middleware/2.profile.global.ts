2413d1bd44c9895526db4e9365bee79b2b48080d
GaoNeng-wWw <gaonenggg@outlook.com>
2025-02-04 20:38:19 +0800

    feat: add user session check in profile middleware

--- middleware/2.profile.global.ts
+++ middleware/2.profile.global.ts
@@ -2,6 +2,10 @@ export default defineNuxtRouteMiddleware(async () => {
   if (import.meta.server){
     return;
   }
+  const {loggedIn} = useUserSession();
+  if (!loggedIn.value){
+    return;
+  }
   const profile = useProfile();
   $fetch('/api/profile')
   .then((realProfile) => profile.value = realProfile)
