71fd77ead505d89bf3125baa58e2863e59b3fdfb
GaoNeng-wWw <gaonenggg@outlook.com>
2025-02-04 15:21:48 +0800

    fix: cursor loss

--- components/ui/markdown/markdown.vue
+++ components/ui/markdown/markdown.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import {Bold, Italic, Strikethrough, ImageUp} from 'lucide-vue-next';
-import { invoke, until } from '@vueuse/shared';
 import {createReusableTemplate} from '@vueuse/core';
+import type { MininalCursor } from '../markdown-editor/markdown-editor.vue';
 const modelValue = defineModel<string>({required: false, default: ''});
 const [defineToolbar,toolbar] = createReusableTemplate()
 const editor = useTemplateRef('editor');
@@ -37,14 +37,19 @@ const renderScroll = () => {
   }
   editorInstance.scrollTop = render.value.scrollTop;
 }
-
+const cursor = reactive({from: 0, to:0});
+const onCursorUpdate = ({from,to}: MininalCursor) => {
+  cursor.from = from;
+  cursor.to = to;
+}
 const baseReplace = (prefix:string,suffix:string) => {
   if (!editor.value){
     return;
   }
   const view = editor.value.getView()!;
-  const {main:{from,to}} = view.state.selection
-  editor.value.replaceSelection(
+  const {from,to} = cursor;
+  editor.value.replace(
+    {from,to},
     `${prefix}${view.state.sliceDoc(from,to)}${suffix}`
   )
 }
@@ -76,11 +81,7 @@ const onUploadFile = (_ev:Event) => {
   .then((handles) => handles.map(createImageMarkdown))
   .then((contents) => {
     contents.forEach((content) => {
-      if (!editor.value || !editor.value.getCursor()){
-        return;
-      }
-      const cursor = editor.value.getCursor();
-      if (!cursor){
+      if (!editor.value){
         return;
       }
       editor.value.insert(cursor,content);
@@ -89,19 +90,6 @@ const onUploadFile = (_ev:Event) => {
   })
 }
 const triggerUpload = () => uploader.value?.click();
-
-invoke(async ()=>{
-  await until<string>(modelValue).toMatch((value) => Boolean(value))
-  if (!editor.value?.ready){
-    watch(editor, ()=>{
-      if (!editor.value?.ready){
-        return;
-      }
-      editor.value.ready();
-    }, {deep: true});
-  }
-  editor.value?.ready();
-})
 </script>
 
 <template>
@@ -126,27 +114,25 @@ invoke(async ()=>{
       </div>
     </define-toolbar>
     <toolbar class="hidden sm:block" />
-    <keep-alive>
-      <ui-tabs class="sm:hidden flex flex-col flex-grow flex-shrink basis-0">
-        <ui-tabs-list default-value="editor" class="w-fit">
-          <ui-tabs-trigger value="editor">Editor</ui-tabs-trigger>
-          <ui-tabs-trigger value="view">View</ui-tabs-trigger>
-        </ui-tabs-list>
-        <ui-tabs-content value="editor" class="flex-col flex-grow flex-shrink basis-0 overflow-auto">
-          <div class="flex flex-col h-full">
-            <toolbar />
-            <div class="flex-grow flex-shrink basis-0 overflow-auto">
-              <ui-markdown-editor ref="editor" v-model="modelValue" />
-            </div>
+    <ui-tabs default-value="editor" class="sm:hidden flex flex-col flex-grow flex-shrink basis-0">
+      <ui-tabs-list class="w-fit">
+        <ui-tabs-trigger value="editor">Editor</ui-tabs-trigger>
+        <ui-tabs-trigger value="view">View</ui-tabs-trigger>
+      </ui-tabs-list>
+      <ui-tabs-content value="editor" class="flex-col flex-grow flex-shrink basis-0 overflow-auto">
+        <div class="flex flex-col h-full">
+          <toolbar />
+          <div class="flex-grow flex-shrink basis-0 overflow-auto">
+            <ui-markdown-editor ref="editor" v-model="modelValue" @cursor-update="onCursorUpdate" />
           </div>
-        </ui-tabs-content>
-        <ui-tabs-content value="view" class="w-full h-full overflow-auto border border-border rounded flex-grow flex-shrink basis-0">
-          <ui-markdown-render :value="modelValue" class="p-2" tag="article" />
-        </ui-tabs-content>
-      </ui-tabs>
-    </keep-alive>
+        </div>
+      </ui-tabs-content>
+      <ui-tabs-content value="view" class="w-full h-full overflow-auto border border-border rounded flex-grow flex-shrink basis-0">
+        <ui-markdown-render :value="modelValue" class="p-2" tag="article" />
+      </ui-tabs-content>
+    </ui-tabs>
     <div class="hidden max-w-full h-full py-2 gap-2 overflow-auto sm:grid grid-cols-2 flex-grow flex-shrink basis-0">
-      <ui-markdown-editor ref="editor" v-model="modelValue" @scroll="editorScroll" />
+      <ui-markdown-editor ref="editor" v-model="modelValue" @cursor-update="onCursorUpdate" @scroll="editorScroll" />
       <div ref="render" class="w-full h-full overflow-auto border border-border rounded" @scroll="renderScroll">
         <ui-markdown-render :value="modelValue" class="p-2" tag="article" />
       </div>
