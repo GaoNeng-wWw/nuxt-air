a13aeb41cacd27e6d181a9bf9ee2ba85aeffd0d3
GaoNeng-wWw <gaonenggg@outlook.com>
2025-02-06 20:44:17 +0800

    feat: add API for site information retrieval and creation

---
+++ server/api/site-info/index.get.ts
@@ -0,0 +1,6 @@
+import { siteInfoKey } from "./index.post";
+
+export default defineApi(async () => {
+  const redis = useRedis();
+  return JSON.parse(await redis.getItem<string>(siteInfoKey) ?? 'null');
+})
