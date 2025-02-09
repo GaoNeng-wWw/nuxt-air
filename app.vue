74a6adbeed725ccd23dbd7316847058a888390df
GaoNeng-wWw <gaonenggg@outlook.com>
2025-02-09 20:24:48 +0800

    fix: scrollTop fail

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <script lang="ts" setup>
 import { Toaster } from 'vue-sonner';
+
+const nuxtApp = useNuxtApp()
+
+nuxtApp.hook("page:finish", () => {
+  document.querySelector('#__nuxt')?.scrollTo(0,0)
+})
 </script>
 
 <template>
