2c8ddfebfbb60f0ccc4172beccaa66facb825952
GaoNeng-wWw <gaonenggg@outlook.com>
2025-02-19 15:53:59 +0800

    feat: debug

--- server/api/post/index.get.ts
+++ server/api/post/index.get.ts
@@ -12,7 +12,7 @@ export default defineEventHandler(async (event) => {
   const { page = 1, size = 20, content, category, publish } = await useQuery(event, PageQuery.merge(GetPostList));
   const { user } = await getUserSession(event);
   // debug
-  console.log(user?.owner, publish);
+  console.log(user, publish);
   const isPublish = user?.owner ? publish : true;
   const posts = await prisma.post.findMany({
     take: size,
