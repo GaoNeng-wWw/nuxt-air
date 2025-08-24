05cf304f55e68565dc2cf735b81edc373ccbaba1
GaoNeng-wWw <gaonenggg@outlook.com>
2025-08-24 12:54:10 +0800

    refactor: `ui-post` rename to `ui-post-item`

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -17,7 +17,7 @@ useSaveScrollPosition(postList);
     <div class="max-w-4xl w-full mx-auto px-8">
       <ul class="space-y-4 h-full">
         <nuxt-link v-for="post of data" :key="post.id" :href="post.path" class="block">
-          <ui-post :post="post" />
+          <ui-post-item :post="post" />
         </nuxt-link>
       </ul>
       <div class="w-full h-fit py-4">
